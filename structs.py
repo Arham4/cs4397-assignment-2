9fb9f4dae0c398642c8979470b49253dcdee9f0b
Chris <christopher.2927@gmail.com>
2021-10-23 13:46:25 -0500

    Modify message queue

--- structs.py
+++ structs.py
@@ -3,10 +3,15 @@
 #MessageQueue is a FIFO queue for communication across tasks or threads
 class MessageQueue:
 
-    queue = []
-
     def push(element):
-        queue.append(element)
+        self._queue.append(element)
 
     def poll():
-        return queue.pop(0)
+        if self._queue.length > 0:
+            return self._queue.pop(0)
+
+    def length():
+        return self._queue.length
+
+    def __init__(self):
+        self._queue = []
